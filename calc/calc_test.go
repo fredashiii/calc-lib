92839a2d2f4ce37572cbb175a6c2db25cb1a779a
Freda Shi <freda@smarty.com>
2025-01-21 16:10:25 -0700

    add some more tests for new calculators

--- calc/calc_test.go
+++ calc/calc_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestCalculateEasy(t *testing.T) {
+func TestCalculateEasyAdd(t *testing.T) {
 	got := Addition{}.Calculate(1, 2)
 	want := 3
 	if got != want {
@@ -12,9 +12,33 @@ func TestCalculateEasy(t *testing.T) {
 	}
 }
 
-func TestCalculateMedium(t *testing.T) {
+func TestCalculateMediumAdd(t *testing.T) {
 	got := Addition{}.Calculate(100, 4)
 	want := 104
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestCalculateEasySubtract(t *testing.T) {
+	got := Subtraction{}.Calculate(1, 2)
+	want := -1
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestCalculateEasyMultiply(t *testing.T) {
+	got := Multiplication{}.Calculate(1, 2)
+	want := 2
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestCalculateEasyDivide(t *testing.T) {
+	got := Division{}.Calculate(4, 2)
+	want := 2
 	if got != want {
 		t.Errorf("got %d, want %d", got, want)
 	}
