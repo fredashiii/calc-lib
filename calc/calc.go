adab5b2f4894efeefb4b1a2f1b8ed7acc4cee36b
Freda Shi <freda@smarty.com>
2025-01-21 16:06:09 -0700

    add more calculators :D

--- calc/calc.go
+++ calc/calc.go
@@ -5,7 +5,22 @@ type Calculator interface {
 }
 
 type Addition struct{}
+type Subtraction struct{}
+type Multiplication struct{}
+type Division struct{}
 
 func (add Addition) Calculate(a, b int) int {
 	return a + b
+}
+
+func (sub Subtraction) Calculate(a, b int) int {
+	return a - b
+}
+
+func (sub Multiplication) Calculate(a, b int) int {
+	return a * b
+}
+
+func (div Division) Calculate(a, b int) int {
+	return a / b
 }
